f5ed3fc1e32bc52e6b42e092fefcf7c63f5ce706
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2026-06-13 11:42:35 +0200

    add lazygit and delta

--- home/default.nix
+++ home/default.nix
@@ -116,6 +116,31 @@ in {
     };
   };
 
+  programs.lazygit = {
+    enable = true;
+    settings.git.pagers = [
+      {
+        colorArg = "always";
+        pager = "delta --paging=never";
+      }
+    ];
+    settings.gui.theme = {
+      selectedLineBgColor = [
+        "black"
+      ];
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      line-numbers = true;
+      navigate = true;
+      theme = "github";
+    };
+  };
+
   services.mpd = {
     enable = true;
     musicDirectory = "/home/gyk/Music/";
