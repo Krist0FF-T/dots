ea855a0b08974baf49f1ea7efcfda3975b370519
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2026-06-24 23:35:44 +0200

    refactor(hosts/gy8): separate out self-hosting stuff

--- hosts/gy8/default.nix
+++ hosts/gy8/default.nix
@@ -1,69 +1,15 @@
-{
-  pkgs,
-  ...
-}: {
+{ ... }: {
   imports = [
     ./hardware-configuration.nix
     ../common.nix
     ./graphics.nix
+    ./self_hosting.nix # nextcloud, tailscale, samba
   ];
 
   networking.hostName = "gy8";
   home-manager.users.gyk = import ../../home;
 
   programs.steam.enable = true;
-
-  # self-hosting - nextcloud, tailscale, samba
-
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud33;
-    hostName = "localhost";
-    config.adminpassFile = "/etc/nextcloud-admin-pass";
-    config.dbtype = "sqlite";
-    settings.trusted_domains = [
-      "*.ts.net"  # for hosting on my tailnet
-      "192.168.*" # for hosting locally
-    ];
-  };
-
-  services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "server";
-  };
-
-  # See https://nixos.wiki/wiki/Samba
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-
-    settings = {
-      global = {
-        "workgroup" = "WORKGROUP";
-        "server string" = "smbnix";
-        "netbios name" = "smbnix";
-        "security" = "user";
-        # ..
-        "hosts allow" = "100. 192.168.0. 127.0.0.1 localhost";
-        "hosts deny" = "0.0.0.0/0";
-        "guest account" = "nobody";
-        "map to guest" = "bad user";
-      };
-      "share" = {
-        "path" = "/mnt/E/share/";
-        "browseable" = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-      };
-    };
-  };
-
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-  };
 
   system.stateVersion = "25.05";
 }
