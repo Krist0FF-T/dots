d6b3955fb9c6096f91283f3847b491767c8fb764
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2026-06-25 12:48:26 +0200

    refactor(system/i18n): remove same as default locale settings

--- hosts/common.nix
+++ hosts/common.nix
@@ -91,17 +91,6 @@
     "hu_HU.UTF-8/UTF-8"
   ];
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # X11 and console keymap
   services.xserver.xkb.layout = "hu";
